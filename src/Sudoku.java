7dc04a933d658737d181858f31fffe35e3302934
927 <glebkalinin927@gmail.com>
2021-01-16 18:17:55 +0300

    Прогресс над судоку: 1/3

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 import java.util.stream.IntStream;
 
@@ -6,7 +7,7 @@ public class Sudoku extends Minigames implements SaveLoad {
 
     private static int[][] origin;
     private static int[][] tmp;
-    private int[][] sudoku;
+    private int[][] grid;
     private int difficulty;
 
     static {
@@ -23,16 +24,62 @@ public class Sudoku extends Minigames implements SaveLoad {
         tmp = origin;
     }
 
-    private static int[][] transpose(int[][] tmp) {
+    public Sudoku(int difficulty){
+        this.difficulty = difficulty;
+        randomize();
+        this.grid = del_num(this.difficulty);
+    }
+
+    public void show(){
+        for(int i = 0; i<9; i++){
+            for(int j = 0; j<9; j++){
+                System.out.printf("|\u203E%s|", this.grid[i][j]==0?" ":this.grid[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    private static void randomize() {
+        Random random = new Random();
+        for (int i = 0; i < 50 + random.nextInt(10); i++) {
+            switch (1 + random.nextInt(4)){
+                case 1: transpose();
+                case 2: swap_rows();
+                case 3: swap_columns();
+                case 4: swap_rows_region();
+                case 5: swap_columns_region();
+            }
+        }
+    }
+    private int[][] del_num(int difficulty){
+        int[][] tmp1 = Arrays.copyOf(tmp, 9);
+        Random random = new Random();
+        int internal_difficulty = difficulty*10;
+        int[][] deleted_numbers = new int[9][9];
+        for(int i = 0; i<internal_difficulty; i++){
+            int x = random.nextInt(9);
+            int y = random.nextInt(9);
+            if (deleted_numbers[x][y] == 1) continue;
+            int tmp2 = tmp1[x][y];
+            tmp1[x][y] = 0;
+            if (!solve(tmp1)) {
+                tmp1[x][y] = tmp2;
+            }
+            deleted_numbers[x][y] = 1;
+        }
+        return tmp1;
+    }
+
+    private static void transpose() {
         int[][] tmp1 = new int[9][9];
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 tmp1[i][j] = tmp[j][i];
             }
         }
-        return tmp1;
+        tmp=tmp1;
     }
-    private static int[][] swap_rows(int[][] tmp) {
+    private static void swap_rows() {
         Random random = new Random();
         int region_num = random.nextInt(3);
         int row1 = random.nextInt(3);
@@ -41,17 +88,17 @@ public class Sudoku extends Minigames implements SaveLoad {
         row1 = region_num * 3 + row1;
         row2 = region_num * 3 + row2;
         for (int i = 0; i < 9; i++) {
-            tmp[row1][i] ^= tmp[row2][i] ^= tmp[row1][i] ^= tmp[row2][i];
+            int tmp2 = tmp[row1][i];
+            tmp[row1][i]=tmp[row2][i];
+            tmp[row2][i]=tmp2;
         }
-        return tmp;
     }
-    private static int[][] swap_columns(int[][] tmp) {
-        tmp = transpose(tmp);
-        tmp = swap_rows(tmp);
-        tmp = transpose(tmp);
-        return tmp;
+    private static void swap_columns() {
+        transpose();
+        swap_rows();
+        transpose();
     }
-    private static int[][] swap_rows_region(int[][] tmp) {
+    private static void swap_rows_region() {
         Random random = new Random();
         int region1 = random.nextInt(3);
         int region2 = random.nextInt(3);
@@ -60,76 +107,76 @@ public class Sudoku extends Minigames implements SaveLoad {
         int c2 = region2 * 3;
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 9; j++) {
-                tmp[c1][j] ^= tmp[c2][j] ^= tmp[c1][j] ^= tmp[c2][j];
+                int tmp2 = tmp[c1][j];
+                tmp[c1][j]=tmp[c2][j];
+                tmp[c2][j]=tmp2;
             }
             c1++;
             c2++;
         }
-        return tmp;
     }
-    private static int[][] swap_columns_region(int[][] tmp) {
-        tmp = transpose(tmp);
-        tmp = swap_rows_region(tmp);
-        tmp = transpose(tmp);
-        return tmp;
+    private static void swap_columns_region() {
+        transpose();
+        swap_rows_region();
+        transpose();
     }
 
     private static boolean solve(int[][] board) {
-        for (int row = 0; row < 9; row++) {
-            for (int column = 0; column < 9; column++) {
-                if (board[row][column] == 0) {
-                    for (int k = 1; k <= 9; k++) {
-                        board[row][column] = k;
-                        if (isValid(board, row, column) && solve(board)) {
-                            return true;
+            for (int row = 0; row < 9; row++) {
+                for (int column = 0; column < 9; column++) {
+                    if (board[row][column] == 0) {
+                        for (int k = 1; k <= 9; k++) {
+                            board[row][column] = k;
+                            if (isValid(board, row, column) && solve(board)) {
+                                board[row][column] = 0;
+                                return true;
+                            }
+                            board[row][column] = 0;
                         }
-                        board[row][column] = 0;
+                        return false;
                     }
+                }
+            }
+            return true;
+        }
+    private static boolean isValid(int[][] board, int row, int column) {
+            return rowConstraint(board, row) &&
+                    columnConstraint(board, column) &&
+                    subsectionConstraint(board, row, column);
+        }
+    private static boolean subsectionConstraint(int[][] board, int row, int column) {
+            boolean[] constraint = new boolean[9];
+            int subsectionRowStart = (row / 3) * 3;
+            int subsectionRowEnd = subsectionRowStart + 3;
+
+            int subsectionColumnStart = (column / 3) * 3;
+            int subsectionColumnEnd = subsectionColumnStart + 3;
+
+            for (int r = subsectionRowStart; r < subsectionRowEnd; r++) {
+                for (int c = subsectionColumnStart; c < subsectionColumnEnd; c++) {
+                    if (!checkConstraint(board, r, constraint, c)) return false;
+                }
+            }
+            return true;
+        }
+    private static boolean columnConstraint(int[][] board, int column) {
+            boolean[] constraint = new boolean[9];
+            return IntStream.range(0, 9)
+                    .allMatch(row -> checkConstraint(board, row, constraint, column));
+        }
+    private static boolean rowConstraint(int[][] board, int row) {
+            boolean[] constraint = new boolean[9];
+            return IntStream.range(0, 9)
+                    .allMatch(column -> checkConstraint(board, row, constraint, column));
+        }
+    private static boolean checkConstraint(int[][] board, int row, boolean[] constraint, int column) {
+            if (board[row][column] != 0) {
+                if (!constraint[board[row][column] - 1]) {
+                    constraint[board[row][column] - 1] = true;
+                } else {
                     return false;
                 }
             }
+            return true;
         }
-        return true;
-    }
-    private static boolean isValid(int[][] board, int row, int column) {
-        return (rowConstraint(board, row)
-                && columnConstraint(board, column)
-                && subsectionConstraint(board, row, column));
-    }
-    private static boolean rowConstraint(int[][] board, int row) {
-        boolean[] constraint = new boolean[81];
-        return IntStream.range(0, 81)
-                .allMatch(column -> checkConstraint(board, row, constraint, column));
-    }
-    private static boolean columnConstraint(int[][] board, int column) {
-        boolean[] constraint = new boolean[81];
-        return IntStream.range(0, 81)
-                .allMatch(row -> checkConstraint(board, row, constraint, column));
-    }
-    private static boolean subsectionConstraint(int[][] board, int row, int column) {
-        boolean[] constraint = new boolean[81];
-        int subsectionRowStart = (row / 3) * 3;
-        int subsectionRowEnd = subsectionRowStart + 3;
-
-        int subsectionColumnStart = (column / 3) * 3;
-        int subsectionColumnEnd = subsectionColumnStart + 3;
-
-        for (int r = subsectionRowStart; r < subsectionRowEnd; r++) {
-            for (int c = subsectionColumnStart; c < subsectionColumnEnd; c++) {
-                if (!checkConstraint(board, r, constraint, c)) return false;
-            }
-        }
-        return true;
-    }
-    private static boolean checkConstraint(int[][] board, int row, boolean[] constraint, int column) {
-        if (board[row][column] != 0) {
-            if (!constraint[board[row][column] - 1]) {
-                constraint[board[row][column] - 1] = true;
-            } else {
-                return false;
-            }
-        }
-        return true;
-    }
-
 }
---
+++ src/qwe.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class qwe {
+    public static void main(String[] args) {
+        Sudoku sudoku = new Sudoku(2);
+        sudoku.show();
+    }
+}
